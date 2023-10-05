63786dcff1e049cd1e0a45386a4d87682fb9335d
FenrisulfrX <wil.edu@gmail.com>
2023-10-05 00:36:35 -0300

    Fix I Tetromino wallKicks

--- scr/Constants.gd
+++ scr/Constants.gd
@@ -14,8 +14,8 @@ const KICK_TABLE = [[Vector2(0,0), Vector2(-1,0), Vector2(-1,-1), Vector2(0,2), 
 						[Vector2(0,0), Vector2(1,0), Vector2(1,-1), Vector2(0,2), Vector2(1,2)], #2->3
 						[Vector2(0,0), Vector2(-1,0), Vector2(-1,1), Vector2(0,-2), Vector2(-1,-2)], #3->0
 ]
-const KICK_TABLE_I = [[Vector2(0,0), Vector2(-2,0), Vector2(1,0), Vector2(-2,-1), Vector2(1,2)], #0->1
-						[Vector2(0,0), Vector2(-1,0), Vector2(2,0), Vector2(-1,2), Vector2(2,-1)], #1->2
-						[Vector2(0,0), Vector2(2,0), Vector2(-1,0), Vector2(2,1), Vector2(-1,-2)], #2->3
-						[Vector2(0,0), Vector2(1,0), Vector2(-2,0), Vector2(1,-2), Vector2(-2,1)], #3->0
+const KICK_TABLE_I = [[Vector2(0,0), Vector2(-2,0), Vector2(1,0), Vector2(-2,1), Vector2(1,-2)], #0->1
+						[Vector2(0,0), Vector2(-1,0), Vector2(2,0), Vector2(-1,-2), Vector2(2,1)], #1->2
+						[Vector2(0,0), Vector2(2,0), Vector2(-1,0), Vector2(2,-1), Vector2(-1,2)], #2->3
+						[Vector2(0,0), Vector2(1,0), Vector2(-2,0), Vector2(1,2), Vector2(-2,-1)], #3->0
 ]
