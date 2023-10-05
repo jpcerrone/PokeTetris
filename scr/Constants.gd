6ad6ba247cb672cf88a041932699af440c4ffeed
FenrisulfrX <wil.edu@gmail.com>
2023-10-05 00:29:54 -0300

    Fix WallKick Values
    Wallkick default values assume the board is going up, reversing Y values fix the wallkicks behaviour

--- scr/Constants.gd
+++ scr/Constants.gd
@@ -9,10 +9,10 @@ const Z_SHAPE = [[6,0,0],[6,6,0],[0,6,0]]
 const S_SHAPE = [[0,7,0],[7,7,0],[7,0,0]]
 
 const SHAPES = [I_SHAPE, J_SHAPE, L_SHAPE, O_SHAPE, T_SHAPE, Z_SHAPE, S_SHAPE]
-const KICK_TABLE = [[Vector2(0,0), Vector2(-1,0), Vector2(-1,1), Vector2(0,-2), Vector2(-1,-2)], #0->1
-						[Vector2(0,0), Vector2(1,0), Vector2(1,-1), Vector2(0,2), Vector2(1,2)], #1->2
-						[Vector2(0,0), Vector2(1,0), Vector2(1,1), Vector2(0,-2), Vector2(1,-2)], #2->3
-						[Vector2(0,0), Vector2(-1,0), Vector2(-1,-1), Vector2(0,2), Vector2(-1,2)], #3->0
+const KICK_TABLE = [[Vector2(0,0), Vector2(-1,0), Vector2(-1,-1), Vector2(0,2), Vector2(-1,2)], #0->1
+						[Vector2(0,0), Vector2(1,0), Vector2(1,1), Vector2(0,-2), Vector2(1,-2)], #1->2
+						[Vector2(0,0), Vector2(1,0), Vector2(1,-1), Vector2(0,2), Vector2(1,2)], #2->3
+						[Vector2(0,0), Vector2(-1,0), Vector2(-1,1), Vector2(0,-2), Vector2(-1,-2)], #3->0
 ]
 const KICK_TABLE_I = [[Vector2(0,0), Vector2(-2,0), Vector2(1,0), Vector2(-2,-1), Vector2(1,2)], #0->1
 						[Vector2(0,0), Vector2(-1,0), Vector2(2,0), Vector2(-1,2), Vector2(2,-1)], #1->2
